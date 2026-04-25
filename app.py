7f7e39cd491646d3487b55ce1f00733a43446ef9
Nihira Shrivastava <nihirashrivastava@gmail.com>
2026-04-25 20:14:44 +0530

    Fixed deployment port issue

--- app.py
+++ app.py
@@ -15,5 +15,8 @@ def home():
 
     return render_template("index.html", translated_text=translated_text)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
